87107c5e2198ced5ab0912df50ec0fe18c370f60
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-14 18:41:54 +0100

    Ciclo while okay

--- js/main.js
+++ js/main.js
@@ -2,39 +2,59 @@
 let arrayList = [ "Insalata", "Pomodori", "Cetrioli", "Zucchine", "Melanzana"];
 
 console.log(arrayList);
-// Andiamo a creare il nostro "containerList".
 
-let containerList = document.createElement("div");
+// Andiamo a creare il ciclo "for"
 
-// Andiamo a creare la "ul".
+// for (let i = 0; i < arrayList.length; i++){
 
-let ulList = document.createElement("ul");
+//     // Andiamo a creare la varibile "element".
 
-// Andiamo ad aggiungerli nella pagine
+//     let elementI = arrayList[i];
+//     // console.log(elementI);
 
-document.getElementsByTagName('body')[0].appendChild(containerList);
-containerList.appendChild(ulList);
+//     // Colleghiamo la nostra lista "ul"
+
+//     let ulList = document.getElementById("ul-list");
+
+//     // Andiamo a creare i "li".
+
+//     let liElement = document.createElement("li");
+//     liElement.append(elementI);
+
+//     // Andiamo ad inserire i "li" a "ul".
+//     ulList.append(liElement);
+
+//     console.log("sull'interazione " + i + " risulta l'elemento " + elementI);
+
+// }
+
+// Andiamo a creare il ciclo "while".
+
+    let i = 0;
+
+    while(i < arrayList.length){
+
+        let elementI = arrayList[i];
+        // console.log(elementI);
+
+        // Colleghia la nostra lista "ul"
+
+        let ulList = document.getElementById("ul-list");
+
+        // Andiamo a creare i "li".
+        let liElement = document.createElement("li");
+        liElement.append(elementI);
+        
+        // Andiamo ad inserire i "li" a "ul".
+        ulList.append(liElement);
+        
+        console.log("sull'interazione " + i + " risulta l'elemento " + elementI);
+
+        i++
+    }
 
 
-    // Creo un ciclo "for".
-    
-    for (let i = 0; i < arrayList.length; i++){
-    
-    // Andiamo a creare la varibile "element".
-    
-    let elementI = arrayList[i];
-    console.log(elementI);
-    
-    // Andiamo a creare i "li".
-    
-    let liElement = document.createElement("li");
-    liElement.append(elementI);
-    
-    // Andiamo ad inserire i "li" a "ul".
-    ulList.append(liElement);
-    
-    // console.log("sull'interazione " + i + " risulta l'elemento " + elementI);
- }
+        
 
 
 

22188fc7e49e45f31bd94c6764637f0c615fe04e
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-14 18:07:53 +0100

    ciclo for

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Andiamo a creare il nostro "arrayList"
-let arrayList =[];
+let arrayList = [ "Insalata", "Pomodori", "Cetrioli", "Zucchine", "Melanzana"];
 
 console.log(arrayList);
 // Andiamo a creare il nostro "containerList".
@@ -15,36 +15,26 @@ let ulList = document.createElement("ul");
 document.getElementsByTagName('body')[0].appendChild(containerList);
 containerList.appendChild(ulList);
 
-// Andiamo a collegare il nostro "bottone".
 
-let myButton = document.getElementById("miobottone");
-
-myButton.addEventListener("click",
-
-    function(){
-
-        let newElement = document.getElementById("nuovoelemento").value;
-        arrayList.push(newElement);
+    // Creo un ciclo "for".
     
-        // Creo un ciclo "for".
-
-        for (let i = 0; i <= arrayList.length; i++){
-
-        // Andiamo a creare la varibile "element".
-        let elementI = arrayList[i];
-        console.log(elementI);
-
-        // Andiamo a creare i "li".
-        let liElement = document.createElement("li");
-        liElement.append(elementI);
-
-        // Andiamo ad inserire i "li" a "ul".
-
-        ulList.append(liElement);
-        
-        // console.log("sull'interazione " + i + " risulta l'elemento " + elementI);
-    }   
-});
+    for (let i = 0; i < arrayList.length; i++){
+    
+    // Andiamo a creare la varibile "element".
+    
+    let elementI = arrayList[i];
+    console.log(elementI);
+    
+    // Andiamo a creare i "li".
+    
+    let liElement = document.createElement("li");
+    liElement.append(elementI);
+    
+    // Andiamo ad inserire i "li" a "ul".
+    ulList.append(liElement);
+    
+    // console.log("sull'interazione " + i + " risulta l'elemento " + elementI);
+ }
 
 
 
